39dcfc5255aee4ca13d5cfe9bfae188c995a1729
jhuynh <79059047+John5675@users.noreply.github.com>
2024-07-30 00:51:06 -0700

    Refactoring Steps project by passing children

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -10,7 +10,9 @@ export default function App() {
   return (
     <div>
       <Steps />
-      <Steps />
+      <StepMessage step={3}>
+        <p>Pass in content</p>
+      </StepMessage>
     </div>
   );
 }
@@ -40,27 +42,49 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          {/* <StepMessage step={step}>{messages[step - 1]}</StepMessage> */}
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#E7E7E7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950F2", color: "#FFF" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950F2", color: "#FFF" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950F2" textColor="#FFF" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+            <Button bgColor="#7950F2" textColor="#FFF" onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3> {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
