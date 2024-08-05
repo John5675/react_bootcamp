63ad3146afce22d7b3a8a4aafb9605c1281e2388
jhuynh <79059047+John5675@users.noreply.github.com>
2024-08-05 00:26:45 -0700

    Completed Half of Nike Website

---
+++ nike/src/sections/SuperQuality.jsx
@@ -0,0 +1,14 @@
+const SuperQuality = () => {
+  return (
+    <section
+      id="about-us"
+      className="max-container flex w-full items-center justify-between gap-10 max-lg:flex-col"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2></h2>
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
